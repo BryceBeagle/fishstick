aafbca2733e9e3174b5bc3b174f882bead97a1a7
ignormies <bryce.beagle@gmail.com>
2022-04-23 09:03:39 -0700

    Update dependencies (#7)

--- software/src/main.rs
+++ software/src/main.rs
@@ -7,7 +7,6 @@ use panic_halt as _;
 use nb::block;
 
 use cortex_m_rt::entry;
-use embedded_hal::digital::v2::OutputPin;
 use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 
 #[entry]
@@ -20,14 +19,14 @@ fn main() -> ! {
     // Take ownership over the raw flash and rcc devices and convert them into the corresponding
     // HAL structs
     let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
+    let rcc = dp.RCC.constrain();
 
     // Freeze the configuration of all the clocks in the system and store the frozen frequencies in
     // `clocks`
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
     // Acquire the GPIOB peripheral
-    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+    let mut gpiob = dp.GPIOB.split();
 
     // Configure pins as push-pull outputs. The `crh` register is passed to the function in order to
     // configure the port. For pins 0-7, crl should be passed instead.
@@ -36,21 +35,22 @@ fn main() -> ! {
     let mut grn_led = gpiob.pb14.into_push_pull_output(&mut gpiob.crh);
 
     // Configure the syst timer to trigger an update every second
-    let mut timer = Timer::syst(cp.SYST, &clocks).start_count_down(1.hz());
+    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
+    timer.start(1.Hz()).unwrap();
 
     // Wait for the timer to trigger an update and change the state of the LED
     loop {
         block!(timer.wait()).unwrap();
-        grn_led.set_high().unwrap();
-        yel_led.set_low().unwrap();
-        red_led.set_low().unwrap();
+        grn_led.set_high();
+        yel_led.set_low();
+        red_led.set_low();
         block!(timer.wait()).unwrap();
-        grn_led.set_low().unwrap();
-        yel_led.set_high().unwrap();
-        red_led.set_low().unwrap();
+        grn_led.set_low();
+        yel_led.set_high();
+        red_led.set_low();
         block!(timer.wait()).unwrap();
-        grn_led.set_low().unwrap();
-        yel_led.set_low().unwrap();
-        red_led.set_high().unwrap();
+        grn_led.set_low();
+        yel_led.set_low();
+        red_led.set_high();
     }
 }
