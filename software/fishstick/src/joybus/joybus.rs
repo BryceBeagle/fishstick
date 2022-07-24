30b6bfddd7f7d96054898ccb7ca22d820d458991
rhedgeco <rhedgeco@gmail.com>
2022-07-24 02:33:55 -0700

    improved joybus method structure

--- software/fishstick/src/joybus/joybus.rs
+++ software/fishstick/src/joybus/joybus.rs
@@ -1,4 +1,12 @@
+use bitvec::prelude::*;
+use bitvec::BitArr;
+
+pub const JOYBUS_SIZE: usize = 128;
+
+pub type Packet = BitArr!(for JOYBUS_SIZE, in u8, Msb0);
+
 pub trait Joybus {
-    fn send(&self, packet: &[i32]) -> Result<(), ()>;
-    fn send_receive<const S: usize>(&self, packet: &[i32]) -> Result<[i32; S], ()>;
+    fn send(&self, packet: &Packet) -> Result<(), ()>;
+    fn send_receive(&self, packet: &Packet) -> Result<&Packet, ()>;
+    fn send_receive_no_alloc(&self, packet: &Packet, receive_packet: &mut Packet) -> Result<(), ()>;
 }
