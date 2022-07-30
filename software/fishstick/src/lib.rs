3424546b2e8531a2fcebd607d74a89497cae7761
ignormies <bryce.beagle@gmail.com>
2022-07-30 13:24:04 -0700

    Sanity-check test for `fishstick` that runs on host machine (#54)

--- software/fishstick/src/lib.rs
+++ software/fishstick/src/lib.rs
@@ -1,4 +1,16 @@
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 
 pub mod gamecube;
 pub mod joybus;
+
+#[cfg(test)]
+mod tests {
+    extern crate alloc;
+    use alloc::vec;
+
+    #[test]
+    fn health_check() -> Result<(), ()> {
+        let _ = vec![1, 2, 3];
+        Ok(())
+    }
+}
