ff351c584e1e5b9569866e50b70c5595b8ee9186
Ryan Hedgecock <rhedgeco@users.noreply.github.com>
2022-11-07 12:27:50 -0800

    improved clock timing

--- software/fishstick-stm32f401/src/main.rs
+++ software/fishstick-stm32f401/src/main.rs
@@ -1,41 +1,46 @@
 #![no_main]
 #![no_std]
 
+mod gamecube;
+
 use embedded_hal::spi::MODE_0;
-use nb::block;
 use panic_rtt_target as _;
-use rtt_target::rtt_init_default;
+use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal::{gpio::NoPin, pac, prelude::*, spi::Spi, timer::Timer};
 
 #[entry]
 fn main() -> ! {
-    rtt_init_default!();
+    rtt_init_print!();
 
+    // get peripherals
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
+    // set up clocks
     let rcc = dp.RCC.constrain();
-
     let clocks = rcc
         .cfgr
         .sysclk(64.MHz())
-        .hclk(32.MHz())
+        .hclk(64.MHz())
         .pclk1(2.MHz())
         .freeze();
 
+    // get pins
     let gpiob = dp.GPIOB.split();
-    let mosi = gpiob.pb15.into_alternate();
-    let mut spi = Spi::new_bidi(dp.SPI2, (NoPin, NoPin, mosi), MODE_0, 1.MHz(), &clocks);
+    let pin_write = gpiob.pb15.into_alternate();
 
-    let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
-    timer.start(60.Hz()).unwrap();
+    // set up SPI
+    let mut spi = Spi::new_bidi(dp.SPI2, (NoPin, NoPin, pin_write), MODE_0, 1.MHz(), &clocks);
 
-    let data = &[0b1010_1010, 0b1010_1010];
+    // set up timer for creating delays
+    let mut timer = Timer::syst(cp.SYST, &clocks).delay();
 
     loop {
-        spi.write(data).unwrap();
-        block!(timer.wait()).unwrap();
+        // write status message to SPI for testing with gamecube controller
+        rprintln!("Sending STATUS_MESSAGE");
+        spi.write(gamecube::STATUS_MESSAGE).unwrap();
+        timer.delay_ms(100u32);
     }
 }
