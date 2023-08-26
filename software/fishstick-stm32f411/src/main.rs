6eb4ce302d9b15e39839e7c93a898f48fb016004
ignormies <bryce.beagle@gmail.com>
2023-08-26 15:19:06 -0700

    Start using `embassy` framework
    Note that this introduces an issue where flashing requires using `BOOT0` and `NRST`
    
    Resolves #66

--- software/fishstick-stm32f411/src/main.rs
+++ software/fishstick-stm32f411/src/main.rs
@@ -1,45 +1,31 @@
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
+#![feature(type_alias_impl_trait)]
 
-use cortex_m_rt::entry;
-use defmt::{info, println};
-#[allow(unused_imports)]
-use defmt_rtt as _; // global_logger
-#[allow(unused_imports)]
-use panic_probe as _; // panic_handler
-use stm32f4xx_hal::{pac, prelude::*};
+use defmt::info;
+#[allow(unused_imports)] // global_logger
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
+#[allow(unused_imports)] // panic_handle
+use panic_probe as _;
 
-#[entry]
-fn main() -> ! {
-    println!("Hello, world!");
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
 
-    if let (Some(dp), Some(cp)) = (
-        pac::Peripherals::take(),
-        cortex_m::peripheral::Peripherals::take(),
-    ) {
-        // Set up the LED. On the Nucleo-446RE it's connected to pin PA5.
-        let gpioc = dp.GPIOC.split();
-        let mut led = gpioc.pc13.into_push_pull_output();
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
 
-        // Set up the system clock. We want to run at 48MHz for this one.
-        let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
+    loop {
+        info!("high");
+        led.set_high();
+        Timer::after(Duration::from_millis(2000)).await;
 
-        // Create a delay abstraction based on SysTick
-        let mut delay = cp.SYST.delay(&clocks);
-
-        loop {
-            // On for 1s, off for 1s.
-            info!("off");
-            led.set_high();
-            delay.delay_ms(1000_u32);
-
-            info!("on");
-            led.set_low();
-            delay.delay_ms(1000_u32);
-        }
+        info!("low");
+        led.set_low();
+        Timer::after(Duration::from_millis(2000)).await;
     }
-
-    loop {}
 }
