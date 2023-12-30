c2ba2ba623b989b04be5b112ffa19e0db5401454
ignormies <bryce.beagle@gmail.com>
2023-12-30 15:28:16 -0800

    Working example of loopback over USB
    Plug in USBC and open Arduino Serial monitor. Anything sent over serial gets sent back in uppercase

--- software/fishstick-stm32f411/src/main.rs
+++ software/fishstick-stm32f411/src/main.rs
@@ -1,30 +1,84 @@
-#![deny(unsafe_code)]
-#![no_main]
+//! CDC-ACM serial port example using polling in a busy loop.
+//! Target board: any STM32F4 with a OTG FS peripheral and a 25MHz HSE crystal
 #![no_std]
+#![no_main]
 
 use defmt::info;
 #[allow(unused_imports)] // global_logger
 use defmt_rtt as _;
-use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::{Duration, Timer};
 #[allow(unused_imports)] // panic_handle
 use panic_probe as _;
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+use cortex_m_rt::entry;
+use stm32f4xx_hal::otg_fs::{UsbBus, USB};
+use stm32f4xx_hal::{pac, prelude::*};
+use usb_device::prelude::*;
 
-    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
+static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+
+#[entry]
+fn main() -> ! {
+    info!("heyyyyyyyyyyyyyy");
+
+    let dp = pac::Peripherals::take().unwrap();
+
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc
+        .cfgr
+        .use_hse(25.MHz())
+        .sysclk(48.MHz())
+        .require_pll48clk()
+        .freeze();
+
+    let gpioa = dp.GPIOA.split();
+
+    let usb = USB::new(
+        (dp.OTG_FS_GLOBAL, dp.OTG_FS_DEVICE, dp.OTG_FS_PWRCLK),
+        (gpioa.pa11, gpioa.pa12),
+        &clocks,
+    );
+
+    let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
+
+    let mut serial = usbd_serial::SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .device_class(usbd_serial::USB_CLASS_CDC)
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Fake Company")
+            .product("Product")
+            .serial_number("TEST")])
+        .unwrap()
+        .build();
 
     loop {
-        info!("high");
-        led.set_high();
-        Timer::after(Duration::from_millis(2000)).await;
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
 
-        info!("low");
-        led.set_low();
-        Timer::after(Duration::from_millis(2000)).await;
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
     }
 }
