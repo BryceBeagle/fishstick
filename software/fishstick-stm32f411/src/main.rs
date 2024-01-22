2e13d77274ecb45c7dd8b5b21cdce6224894cc44
Ryan Hedgecock <rhedgeco@gmail.com>
2024-01-21 17:52:23 -0800

    upload of usb mouse twitch test

--- software/fishstick-stm32f411/src/main.rs
+++ software/fishstick-stm32f411/src/main.rs
@@ -11,19 +11,46 @@ use panic_probe as _;
 
 use cortex_m_rt::entry;
 use stm32f4xx_hal::otg_fs::{UsbBus, USB};
-use stm32f4xx_hal::{pac, prelude::*};
+use stm32f4xx_hal::{interrupt, pac, prelude::*};
+use usb_device::class_prelude::*;
 use usb_device::prelude::*;
+use usbd_hid::descriptor::{generator_prelude::*, MouseReport};
+use usbd_hid::hid_class::HIDClass;
+
+use cortex_m::asm::delay as cycle_delay;
+use cortex_m::interrupt::free as disable_interrupts;
+use cortex_m::peripheral::NVIC;
 
 static mut EP_MEMORY: [u32; 1024] = [0; 1024];
+
+static mut USB_ALLOC: Option<UsbBusAllocator<UsbBus<USB>>> = None;
+static mut USB_DEVICE: Option<UsbDevice<UsbBus<USB>>> = None;
+static mut USB_HID: Option<HIDClass<UsbBus<USB>>> = None;
+
+fn push_mouse_movement(report: MouseReport) -> Result<usize, usb_device::UsbError> {
+    disable_interrupts(|_| unsafe { USB_HID.as_mut().map(|hid| hid.push_input(&report)) }).unwrap()
+}
+
+fn poll_usb() {
+    unsafe {
+        if let (Some(usb_device), Some(usb_hid)) = (USB_DEVICE.as_mut(), USB_HID.as_mut()) {
+            usb_device.poll(&mut [usb_hid]);
+        }
+    };
+}
+
+#[interrupt]
+fn OTG_FS() {
+    poll_usb();
+}
 
 #[entry]
 fn main() -> ! {
     info!("heyyyyyyyyyyyyyy");
 
-    let dp = pac::Peripherals::take().unwrap();
-
-    let rcc = dp.RCC.constrain();
-
+    let peripherals = pac::Peripherals::take().unwrap();
+    let mut core = pac::CorePeripherals::take().unwrap();
+    let rcc = peripherals.RCC.constrain();
     let clocks = rcc
         .cfgr
         .use_hse(25.MHz())
@@ -31,54 +58,63 @@ fn main() -> ! {
         .require_pll48clk()
         .freeze();
 
-    let gpioa = dp.GPIOA.split();
+    let gpioa = peripherals.GPIOA.split();
 
     let usb = USB::new(
-        (dp.OTG_FS_GLOBAL, dp.OTG_FS_DEVICE, dp.OTG_FS_PWRCLK),
+        (
+            peripherals.OTG_FS_GLOBAL,
+            peripherals.OTG_FS_DEVICE,
+            peripherals.OTG_FS_PWRCLK,
+        ),
         (gpioa.pa11, gpioa.pa12),
         &clocks,
     );
 
-    let usb_bus = UsbBus::new(usb, unsafe { &mut EP_MEMORY });
+    unsafe {
+        USB_ALLOC = Some(UsbBus::new(usb, &mut EP_MEMORY));
 
-    let mut serial = usbd_serial::SerialPort::new(&usb_bus);
+        USB_HID = Some(HIDClass::new(
+            USB_ALLOC.as_ref().unwrap(),
+            MouseReport::desc(),
+            60,
+        ));
 
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .device_class(usbd_serial::USB_CLASS_CDC)
-        .strings(&[StringDescriptors::default()
-            .manufacturer("Fake Company")
-            .product("Product")
-            .serial_number("TEST")])
-        .unwrap()
-        .build();
+        USB_DEVICE = Some(
+            UsbDeviceBuilder::new(USB_ALLOC.as_ref().unwrap(), UsbVidPid(0x16c0, 0x27dd))
+                .strings(&[StringDescriptors::default()
+                    .manufacturer("Fake Company")
+                    .product("Product")
+                    .serial_number("TEST")])
+                .unwrap()
+                .build(),
+        );
+    }
+
+    unsafe {
+        core.NVIC.set_priority(interrupt::OTG_FS, 1);
+        NVIC::unmask(interrupt::OTG_FS);
+    }
 
     loop {
-        if !usb_dev.poll(&mut [&mut serial]) {
-            continue;
-        }
-
-        let mut buf = [0u8; 64];
-
-        match serial.read(&mut buf) {
-            Ok(count) if count > 0 => {
-                // Echo back in upper case
-                for c in buf[0..count].iter_mut() {
-                    if 0x61 <= *c && *c <= 0x7a {
-                        *c &= !0x20;
-                    }
-                }
-
-                let mut write_offset = 0;
-                while write_offset < count {
-                    match serial.write(&buf[write_offset..count]) {
-                        Ok(len) if len > 0 => {
-                            write_offset += len;
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            _ => {}
-        }
+        cycle_delay(25 * 1024 * 1024);
+        push_mouse_movement(MouseReport {
+            x: 0,
+            y: 4,
+            buttons: 0,
+            pan: 0,
+            wheel: 0,
+        })
+        .ok()
+        .unwrap_or(0);
+        cycle_delay(25 * 1024 * 1024);
+        push_mouse_movement(MouseReport {
+            x: 0,
+            y: -4,
+            buttons: 0,
+            pan: 0,
+            wheel: 0,
+        })
+        .ok()
+        .unwrap_or(0);
     }
 }
